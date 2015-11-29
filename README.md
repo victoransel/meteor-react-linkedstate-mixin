8ac05c0b75a2bd12cd55085626a7f70013a2c75c
Victor Ansel <v.ansel91@gmail.com>
2015-11-29 14:27:58 -0200

    Init repo

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# meteor-react-linkedstate-mixin
-LinkedStateMixin package for meteorjs
+# react-linkedstate-mixin
+
+[react-linkedstate-mixin](https://www.npmjs.com/package/react-addons-linked-state-mixin) packaged for [Meteor](https://meteor.com).
+
+## Installation
+
+    meteor add victoransel:react-linkedstate-mixin
+
+## Example
+
+```js
+var Component = React.createClass({
+  mixins: [LinkedStateMixin],
+  getInitialState: function() {
+    return {message: 'Hello!'};
+  },
+  render: function() {
+    return <input type="text" valueLink={this.linkState('message')} />;
+  }
+});
+```
---
+++ package.js
@@ -0,0 +1,29 @@
+Package.describe({
+  name: 'victoransel:react-linkedstate-mixin',
+  version: '0.13.2',
+  summary: 'ReactLink is an easy way to express two-way binding with React.',
+  git: 'https://github.com/codeempathy/meteor-react-timer-mixin',
+  documentation: 'README.md'
+});
+
+Npm.depends({
+  "react-addons-linked-state-mixin": "0.14.3"
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.2.1');
+  api.use('ecmascript');
+  api.use('cosmos:browserify@0.8.3');
+  api.addFiles('react-linkedstate-mixin.browserify.js');
+
+  api.export('LinkedStateMixin');
+});
+
+Package.onTest(function(api) {
+  api.use('ecmascript');
+  api.use('tinytest');
+  api.use('react@0.14.1');
+  api.use('jsx@0.2.3');
+  api.use('victoransel:react-linkedstate-mixin');
+  api.addFiles('react-linkedstate-mixin-tests.jsx', 'client');
+});
